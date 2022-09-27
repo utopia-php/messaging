cb69920d7d15f1b8b1db84eb92e45772ee4593e8
Jake Barnby <jakeb994@gmail.com>
2022-09-27 15:39:58 +1300

    Add SMS message type

---
+++ src/Messaging/SMS/SMSMessage.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Messaging\SMS;
+
+use Messaging\Message;
+
+class SMSMessage implements Message
+{
+    public function __construct(
+        private array   $to,
+        private string  $content,
+        private ?string $from = null,
+        private ?array  $attachments = null,
+    )
+    {
+    }
+
+    /**
+     * @return array
+     */
+    public function getTo(): array
+    {
+        return $this->to;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFrom(): ?string
+    {
+        return $this->from;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getAttachments(): ?array
+    {
+        return $this->attachments;
+    }
+
+
+}
