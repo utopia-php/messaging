13f82ef8c5e4a3e01335c04c258be46d4bdfb63c
Jake Barnby <jakeb994@gmail.com>
2022-09-27 15:39:26 +1300

    Add mailgun adapter

---
+++ src/Messaging/Email/Mailgun.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Messaging\Email;
+
+class Mailgun extends EmailAdapter
+{
+    /**
+     * @param string $apiKey Your Mailgun API key to authenticate with the API.
+     * @param string $domain Your Mailgun domain to send messages from.
+     */
+    public function __construct(
+        private string $apiKey,
+        private string $domain,
+    )
+    {
+    }
+
+    public function getName(): string
+    {
+        return 'Mailgun';
+    }
+
+    public function getMaxMessagesPerRequest(): int
+    {
+        return 1000;
+    }
+
+    /**
+     * @inheritdoc
+     * @throws \Exception
+     */
+    protected function sendMessage(EmailMessage $message): string
+    {
+        return $this->request(
+            method: 'POST',
+            url: "https://api.mailgun.net/v3/{$this->domain}/messages",
+            headers: [
+                'Authorization: Basic ' . base64_encode('api:' . $this->apiKey)
+            ],
+            body: \http_build_query([
+                'from' => $message->getFrom(),
+                'to' => \join(',', $message->getTo()),
+                'subject' => $message->getSubject(),
+                'text' => $message->isHtml() ? null : $message->getContent(),
+                'html' => $message->isHtml() ? $message->getContent() : null,
+            ]),
+        );
+    }
+}
