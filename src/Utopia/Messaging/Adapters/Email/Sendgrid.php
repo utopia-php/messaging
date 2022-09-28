bcb67aeac1088afe92ba3e9354fb777900203147
Jake Barnby <jakeb994@gmail.com>
2022-09-28 15:14:36 +1300

    Add Sendgrid email adapter

---
+++ src/Utopia/Messaging/Adapters/Email/Sendgrid.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Utopia\Messaging\Adapters\Email;
+
+use Utopia\Messaging\Messages\Email;
+
+class Sendgrid extends Base
+{
+    public function __construct(
+        private string $apiKey,
+    ) {
+    }
+
+    public function getName(): string
+    {
+        return 'Sendgrid';
+    }
+
+    public function getMaxMessagesPerRequest(): int
+    {
+        return 1000;
+    }
+
+    protected function sendMessage(Email $message): string
+    {
+        return $this->request(
+            method: 'POST',
+            url: 'https://api.sendgrid.com/v3/mail/send',
+            headers: [
+                'Authorization: Bearer ' . $this->apiKey,
+                'Content-Type: application/json',
+            ],
+            body: \json_encode([
+                'personalizations' => [
+                    [
+                        'to' => \array_map(
+                            fn($to) => ['email' => $to],
+                            $message->getTo()
+                        ),
+                        'subject' => $message->getSubject(),
+                    ],
+                ],
+                'from' => [
+                    'email' => $message->getFrom(),
+                ],
+                'content' => [
+                    [
+                        'type' => $message->isHtml() ? 'text/html' : 'text/plain',
+                        'value' => $message->getContent(),
+                    ],
+                ],
+            ]),
+        );
+    }
+}
