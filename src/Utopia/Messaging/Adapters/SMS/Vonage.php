93552df09887316e24bc7104270c392f8b054a3f
Jake Barnby <jakeb994@gmail.com>
2022-09-28 14:44:01 +1300

    Add Vonage SMS adapter

---
+++ src/Utopia/Messaging/Adapters/SMS/Vonage.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Utopia\Messaging\Adapters\SMS;
+
+// Reference Material
+// https://www.textmagic.com/docs/api/send-sms/#How-to-send-bulk-text-messages
+
+use Utopia\Messaging\Messages\SMS;
+
+class Vonage extends Base
+{
+    /**
+     * @param string $apiKey Vonage API Key
+     * @param string $apiSecret Vonage API Secret
+     */
+    public function __construct(
+        private string $apiKey,
+        private string $apiSecret
+    )
+    {
+    }
+
+    public function getName(): string
+    {
+        return 'Vonage';
+    }
+
+    public function getMaxMessagesPerRequest(): int
+    {
+        return 1;
+    }
+
+    /**
+     * @inheritdoc
+     * @throws \Exception
+     */
+    protected function sendMessage(SMS $message): string
+    {
+        return $this->request(
+            method: 'POST',
+            url: 'https://rest.nexmo.com/sms/json',
+            body: [
+                'text' => $message->getContent(),
+                'from' => $message->getFrom(),
+                'to' => \implode(',', $message->getTo()),
+                'api_key' => $this->apiKey,
+                'api_secret' => $this->apiSecret
+            ]
+        );
+    }
+}
