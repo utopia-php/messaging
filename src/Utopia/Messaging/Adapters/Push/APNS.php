52021eef254ac5a04947a9ea81c91c796f63d71c
wess <me@wess.io>
2023-07-19 09:36:11 -0400

    Added start to APNS

---
+++ src/Utopia/Messaging/Adapters/Push/APNS.php
@@ -0,0 +1,33 @@
+<?php
+
+use Utopia\Messaging\Adapters\Push as PushAdapter;
+use Utopia\Messaging\Apple\Notification as ApplePushNotification;
+use Utopia\Messaging\Messages\Push;
+
+class ApplePushAdapter implements PushAdapter
+{
+    protected $client;
+
+    public function __construct($certificatePath, $passphrase, $ssl = 'ssl://gateway.push.apple.com:2195')
+    {
+        $this->client = new ApplePushNotification($certificatePath, $passphrase, $ssl);
+    }
+
+    public function getName(): string
+    {
+        return 'applePush';
+    }
+
+    public function process(Push $message): string
+    {
+        try {
+            $this->client->send($message->getTo(), $message->getBody());
+
+            return true;
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage(), 500);
+        }
+    }
+}
+
+?>
