a9e900002c94da7ec7fb7b3967b6b59238c4e6b2
wess <me@wess.io>
2023-09-11 13:07:26 -0400

    Adds FCM_SERVER_TO back in to ENV

--- tests/e2e/Push/FCMTest.php
+++ tests/e2e/Push/FCMTest.php
@@ -13,7 +13,7 @@ class FCMTest extends Base
 
         $adapter = new FCMAdapter($serverKey);
 
-        $to = "eJa9AhokQUudfBPJwRx2OX:APA91bE0KbMkXU7a4eCyq1CyN1nR9TwOD5NQIaHADJBMBV1GjOjTfyPywOXKVeKVvvjz6nvB2jASGtRxGJHsM4Z4osoHnTx5IrnxCNUDEH11wsm4vMBiKW0zbugVis1MdtusTu9admrk"; //getenv('FCM_SERVER_TO');
+        $to = getenv('FCM_SERVER_TO');
 
         $message = new Push(
             to: [$to],
