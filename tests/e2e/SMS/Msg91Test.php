fd88522db3cb852aa62af3b60790510cef9f8f89
loks0n <22452787+loks0n@users.noreply.github.com>
2023-11-09 12:24:48 +0000

    test: fix msg91

--- tests/e2e/SMS/Msg91Test.php
+++ tests/e2e/SMS/Msg91Test.php
@@ -13,18 +13,17 @@ class Msg91Test extends Base
     public function testSendSMS()
     {
         $sender = new Msg91(getenv('MSG_91_SENDER_ID'), getenv('MSG_91_AUTH_KEY'));
+        $sender->setTemplate(getenv('MSG_91_FROM'));
 
         $message = new SMS(
             to: [getenv('MSG_91_TO')],
             content: 'Test Content',
-            from: getenv('MSG_91_FROM')
         );
 
         $response = $sender->send($message);
         $result = \json_decode($response, true);
 
         $this->assertEquals('success', $result['type']);
-
-        // $this->markTestSkipped('Msg91 requires business verification to use template and SMS api.');
+        $this->assertNotEmpty($result['message']);
     }
 }
