d2786987ae2eec839507a1293287da9cece1d134
Chaitanya7666 <chaitu.y25@gmail.com>
2024-03-13 14:06:32 +0530

    Added: Semaphore SMS adapter

--- src/Utopia/Messaging/Adapter/SMS/Semaphore.php
+++ src/Utopia/Messaging/Adapter/SMS/Semaphore.php
@@ -13,19 +13,13 @@ class Semaphore extends SMSAdapter
 {
     protected const NAME = 'Semaphore';
 
-        /**
+    /**
      * @param  string  $apikey Semaphore api key
-     * @param  string  $number recipient number(s). If multiple numnbers, split with comma.
-     * @param  string  $message message you want to send
-     * @param  string  $sendername What your recipient sees as the sender of your message
      */
 
-     
+
     public function __construct(
-        private string $apikey,
-        private string $number,
-        private string $message,
-        private string $senderName
+        private string $apikey
     ) {
     }
 
@@ -36,17 +30,15 @@ class Semaphore extends SMSAdapter
 
     public function getMaxMessagesPerRequest(): int
     {
-        // TODO: Find real limit
-        return 25;
+        // NOTE: user can do upto 1000 numbers per API call
+        return 1000;
     }
 
     /**
      * {@inheritdoc}
      */
-    protected function process(SMSMessage $message): array
+    public function process(SMSMessage $message): array
     {
-     
-
         $response = new Response($this->getType());
         $result = $this->request(
             method: 'POST',
@@ -56,21 +48,28 @@ class Semaphore extends SMSAdapter
             ],
             body: [
                 'apikey' => $this->apikey,
-                'number' => $this->number,
-                'message' => $this->message,
-                'senderName' => $this->senderName
+                'number' => $message->getTo()[0],
+                'message' => $message->getContent(),
+                'sendername' => $message->getFrom()
             ],
         );
 
         if ($result['statusCode'] === 200) {
-            $response->setDeliveredTo(\count($message->getTo()));
-            foreach ($message->getTo() as $to) {
-                $response->addResult($to);
+            if ($result['response'][0] && count($result['response'][0]) > 1) {
+                $response->addResult($message->getTo()[0]);
+            } else {
+                foreach ($result['response'] as $variableName) {
+                    $errorMessage = $variableName;
+                    if (is_array($variableName)) {
+                        $response->addResult($message->getTo()[0], $errorMessage[0]);
+                    } else {
+                        $response->addResult($message->getTo()[0], 'Unknown error');
+                    }
+                }
             }
-        } else {
-            foreach ($message->getTo() as $to) {
-                $response->addResult($to, 'Unknown error');
-            }
+        }
+        if ($result['statusCode'] === 500) {
+            $response->addResult($message->getTo()[0], $result['response'][0]);
         }
 
         return $response->toArray();
@@ -78,14 +77,59 @@ class Semaphore extends SMSAdapter
 }
 
 
-// The following lines of code are used to instantiate an object for the above class
-$apikey='jkjdkfafd';
-$number = '07358574402';
-$message = 'Nice meeting you';
-$senderName = 'Chaitanya';
+// Below is a Sample Error response for bad payload
+// The status code is 200 even if there is an error.
+// The status code is 200 if semaphore returns a response irrespective of good or error response
+
+// $errorResponse = array(
+//     "url" => "https://api.semaphore.co/api/v4/messages",
+//     "statusCode" => 200,
+//     "response" => array(
+//         "sendername" => array(
+//             "The selected sendername is invalid."
+//         )
+//     ),
+//     "error" => ""
+// );
 
 
-$semaphore = new Semaphore($apikey, $number, $message, $senderName);
+
+// Below is a Sample Error response when Semaphore credits are empty
+
+// $errorResponse = [
+//     "url" => "https://api.semaphore.co/api/v4/messages",
+//     "statusCode" => 500,
+//     "response" => [
+//         "Your current balance of 0 credits is not sufficient. This transaction requires 1 credits."
+//     ],
+//     "error" => ""
+// ];
 
 
-echo $semaphore;
+
+// Below is a sample success response
+// Unlike error response, More than 1 keys are returned in the response array. Refer to docs
+
+// $successResponse = array(
+//     "url" => "https://api.semaphore.co/api/v4/messages",
+//     "statusCode" => 200,
+//     "response" => array(
+//         array(
+//             "message_id" => 212210271,
+//             "user_id" => 40495,
+//             "user" => "hello@gmail.com",
+//             "account_id" => 40356,
+//             "account" => "Semiphore",
+//             "recipient" => "639358574402",
+//             "message" => "Nice meeting you",
+//             "sender_name" => "Semaphore",
+//             "network" => "Globe",
+//             "status" => "Pending",
+//             "type" => "Single",
+//             "source" => "Api",
+//             "created_at" => "2024-03-12 23:14:50",
+//             "updated_at" => "2024-03-12 23:14:50"
+//         )
+//     ),
+//     "error" => ""
+// );
