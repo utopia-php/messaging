be5507b40ef115ba980172fc0fa8e21522845b60
Jake Barnby <jakeb994@gmail.com>
2024-12-05 20:53:24 +1300

    Add more push param tests

---
+++ tests/Messaging/Adapter/Push/Base.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Utopia\Tests\Adapter\Push;
+
+use Utopia\Messaging\Adapter;
+use Utopia\Messaging\Adapter\Push\APNS as APNSAdapter;
+use Utopia\Messaging\Messages\Push;
+use Utopia\Messaging\Priority;
+use Utopia\Tests\Adapter\Base as TestBase;
+
+abstract class Base extends TestBase
+{
+    private Adapter $adapter;
+
+    protected abstract function getTo(): array;
+
+    public function testSend(): void
+    {
+        $message = new Push(
+            to: $this->getTo(),
+            title: 'Test title',
+            body: 'Test body',
+            data: null,
+            action: null,
+            sound: 'default',
+            icon: null,
+            color: null,
+            tag: null,
+            badge: 1,
+        );
+
+        $response = $this->adapter->send($message);
+
+        $this->assertResponse($response);
+    }
+
+    public function testSendSilent(): void
+    {
+        $message = new Push(
+            to: $this->getTo(),
+            data: [
+                'key' => 'value',
+            ],
+            contentAvailable: true
+        );
+
+        $response = $this->adapter->send($message);
+
+        $this->assertResponse($response);
+    }
+
+    public function testSendCritical(): void
+    {
+        $message = new Push(
+            to: $this->getTo(),
+            title: 'Test title',
+            body: 'Test body',
+            critical: true
+        );
+
+        $response = $this->adapter->send($message);
+
+        $this->assertResponse($response);
+    }
+
+    public function testSendPriority(): void
+    {
+        $message = new Push(
+            to: $this->getTo(),
+            title: 'Test title',
+            body: 'Test body',
+            priority: Priority::HIGH
+        );
+
+        $response = $this->adapter->send($message);
+
+        $this->assertResponse($response);
+    }
+}
--- tests/Messaging/Adapter/Push/FCMTest.php
+++ tests/Messaging/Adapter/Push/FCMTest.php
@@ -3,34 +3,20 @@
 namespace Utopia\Tests\Adapter\Push;
 
 use Utopia\Messaging\Adapter\Push\FCM as FCMAdapter;
-use Utopia\Messaging\Messages\Push;
-use Utopia\Tests\Adapter\Base;
 
 class FCMTest extends Base
 {
-    public function testSend(): void
+    protected function setUp(): void
     {
+        parent::setUp();
+
         $serverKey = \getenv('FCM_SERVICE_ACCOUNT_JSON');
 
-        $adapter = new FCMAdapter($serverKey);
+        $this->adapter = new FCMAdapter($serverKey);
+    }
 
-        $to = \getenv('FCM_TO');
-
-        $message = new Push(
-            to: [$to],
-            title: 'Test title',
-            body: 'Test body',
-            data: null,
-            action: null,
-            sound: 'default',
-            icon: null,
-            color: null,
-            tag: null,
-            badge: 1
-        );
-
-        $response = $adapter->send($message);
-
-        $this->assertResponse($response);
+    protected function getTo(): array
+    {
+        return [\getenv('FCM_TO')];
     }
 }
