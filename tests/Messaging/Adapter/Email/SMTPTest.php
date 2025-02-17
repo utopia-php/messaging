01736ac71f7b4e142f4372460b6e683e7b492d1a
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2025-02-17 11:01:14 +0530

    chore: fix tests

--- tests/Messaging/Adapter/Email/SMTPTest.php
+++ tests/Messaging/Adapter/Email/SMTPTest.php
@@ -27,13 +27,7 @@ class SMTPTest extends Base
             subject: $subject,
             content: $content,
             fromName: $fromName,
-            fromEmail: $fromEmail,
-            bcc: [
-                [
-                    'email' => 'tester2@localhost.test',
-                    'name' => 'Test Recipient 2',
-                ],
-            ],
+            fromEmail: $fromEmail
         );
 
         $response = $sender->send($message);
